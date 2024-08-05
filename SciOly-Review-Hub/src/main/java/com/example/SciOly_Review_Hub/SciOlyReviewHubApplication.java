4366cb383e77bf769bebd2acd86ade2d329962db
Brandon Ding <dexon888@gmail.com>
2024-08-04 22:55:47 -0700

    Initialized SpringBoot backend and React frontend

---
+++ SciOly-Review-Hub/src/main/java/com/example/SciOly_Review_Hub/SciOlyReviewHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SciOly_Review_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SciOlyReviewHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SciOlyReviewHubApplication.class, args);
+	}
+
+}
---
+++ SciOly-Review-Hub/src/test/java/com/example/SciOly_Review_Hub/SciOlyReviewHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SciOly_Review_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SciOlyReviewHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
